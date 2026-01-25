381de560fc7cbdb7f84b54114c5ce3762ecfb812
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-25 17:55:55 +0100

    .

--- js.js
+++ js.js
@@ -11,3 +11,12 @@ window.addEventListener("scroll", () => {
     marquee.classList.add("opacityAnimation");
   } else marquee.classList.remove("opacityAnimation");
 });
+function setFullHeight() {
+  const vh = window.innerHeight;
+  document.querySelectorAll(".size-custom, .bg-custom::before").forEach((el) => {
+    el.style.height = `${vh}px`;
+  });
+}
+
+window.addEventListener("resize", setFullHeight);
+window.addEventListener("load", setFullHeight);
