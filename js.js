f093bfa2386787343d7c6c07b109d806bf1a9f88
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-25 20:51:07 +0100

    added blur time out

--- js.js
+++ js.js
@@ -14,6 +14,8 @@ window.addEventListener("scroll", () => {
 
 const btnUp = document.querySelector("#btnUp");
 const btnDown = document.querySelector("#btnDown");
+const blurredScreen =
+  "https://images.unsplash.com/photo-1765445773906-64a36ecfd20e?q=80&w=1669&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 const work1 =
   "https://images.unsplash.com/photo-1768772123991-b17e721119a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8";
 const work2 =
@@ -24,19 +26,32 @@ const work4 =
   "https://plus.unsplash.com/premium_photo-1753982281975-a909c3014db6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D";
 const works = [work1, work2, work3, work4];
 let counter = 0;
+let pressed = false;
 const switchPhotoUp = () => {
-  const worksPhoto = document.querySelector("#worksPhoto");
-  counter++;
-  if (counter > 3) counter = 0;
-  worksPhoto.src = works[counter];
-  console.log(counter);
+  if (!pressed) {
+    pressed = true;
+    counter++;
+    if (counter > 3) counter = 0;
+    const worksPhoto = document.querySelector("#worksPhoto");
+    worksPhoto.src = blurredScreen;
+    setTimeout(() => {
+      worksPhoto.src = works[counter];
+      pressed = false;
+    }, 500);
+  }
 };
 const switchPhotoDown = () => {
-  const worksPhoto = document.querySelector("#worksPhoto");
-  counter--;
-  if (counter < 0) counter = 3;
-  worksPhoto.src = works[counter];
-  console.log(counter);
+  if (!pressed) {
+    pressed = true;
+    counter--;
+    if (counter < 0) counter = 3;
+    const worksPhoto = document.querySelector("#worksPhoto");
+    worksPhoto.src = blurredScreen;
+    setTimeout(() => {
+      worksPhoto.src = works[counter];
+      pressed = false;
+    }, 500);
+  }
 };
 
 btnUp.addEventListener("click", () => {
