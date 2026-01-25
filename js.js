1108f4765986a805b1b5b9e306d046486f94c599
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-25 16:24:20 +0100

    test overscroll

--- js.js
+++ js.js
@@ -5,3 +5,20 @@ document.fonts.ready.then(() => {
     marquee.classList.add("marquee-ready");
   }, 1000);
 });
+const body = document.body;
+
+body.addEventListener(
+  "touchmove",
+  (e) => {
+    const scrollTop = body.scrollTop || document.documentElement.scrollTop;
+    const scrollHeight = body.scrollHeight;
+    const clientHeight = window.innerHeight;
+
+    // se in cima e scroll down
+    if (scrollTop <= 0 && e.touches[0].clientY > 0) e.preventDefault();
+
+    // se in fondo e scroll up
+    if (scrollTop + clientHeight >= scrollHeight && e.touches[0].clientY < 0) e.preventDefault();
+  },
+  { passive: false },
+);
