28782a2c11c7e6d70788208f72362940870a827b
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-25 16:20:16 +0100

    1s timer animation span

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const marquee = document.querySelector(".marquee-track");
 document.fonts.ready.then(() => {
   setTimeout(() => {
     marquee.classList.add("marquee-ready");
-  }, 2000);
+  }, 1000);
 });
