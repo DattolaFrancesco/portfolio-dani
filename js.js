9ce85e586d64280283e11d11c98535fcf3f61cc8
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-01 15:57:35 +0100

    test

--- js.js
+++ js.js
@@ -15,38 +15,37 @@ window.addEventListener("scroll", () => {
 const btnUp = document.querySelector("#btnUp");
 const btnDown = document.querySelector("#btnDown");
 const btnPush = document.querySelector("#btnPush");
-const blurredScreen = "./img/blur.jpg";
+const blurredScreen = "./img/blur.gif";
 const work1 = "img/Queio_1.png";
-const work2 =
-  "https://plus.unsplash.com/premium_photo-1768053968250-c1ea4a302653?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8";
-const work3 =
-  "https://images.unsplash.com/photo-1768933294181-82778103e501?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D";
-const work4 =
-  "https://plus.unsplash.com/premium_photo-1753982281975-a909c3014db6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D";
-const works = [work1, work2, work3, work4];
+const work2 = "img/2_Vetrina.png";
+const work3 = "img/3_Vetrina.png";
+const work4 = "img/4_Lettering.png";
+const work5 = "img/5_Character.png";
+const work6 = "img/6_Misc..png";
+const works = [work1, work2, work3, work4, work5, work6];
 let counter = 0;
 let pressed = false;
 const switchPhotoUp = () => {
   pressed = true;
   counter++;
-  if (counter > 3) counter = 0;
+  if (counter > 5) counter = 0;
   const worksPhoto = document.querySelector("#worksPhoto");
   worksPhoto.src = blurredScreen;
   setTimeout(() => {
     worksPhoto.src = works[counter];
     pressed = false;
-  }, 500);
+  }, 200);
 };
 const switchPhotoDown = () => {
   pressed = true;
   counter--;
-  if (counter < 0) counter = 3;
+  if (counter < 0) counter = 5;
   const worksPhoto = document.querySelector("#worksPhoto");
   worksPhoto.src = blurredScreen;
   setTimeout(() => {
     worksPhoto.src = works[counter];
     pressed = false;
-  }, 500);
+  }, 200);
 };
 
 btnUp.addEventListener("click", () => {
