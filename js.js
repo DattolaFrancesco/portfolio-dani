7f5e8f9aead673878141f9e0dfa6b96568398714
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-08 15:11:48 +0100

    .

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 const marquee = document.querySelector(".marquee-track");
 
-const works = ["img/1_QUEIO.png", "img/2_Character.png", "img/4_LETTERING.png", "img/3_Vetrina.png", "img/2_Popup.png", "img/6_Misc..png"];
+const works = ["img/1_QUEIO.png", "img/2_character.png", "img/4_LETTERING.png", "img/3_Vetrina.png", "img/2_Popup.png", "img/6_Misc..png"];
 
 const blurredScreen = "./img/blur.gif";
 const blurredImg = new Image();
